bfe3043af57d8040a2cd71c037911fefd1f8fba4
dev-araujo <theriancirculo@protonmail.com>
2024-03-06 17:30:40 -0300

    chore: cors config

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/", routes);
 
 const PORT = process.env.PORT || 3000;
